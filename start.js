29477a3b9c2f0ff16665ced13915fcc3574b5923
alexilaih0 <alexi.h.laih0@gmail.com>
2015-11-27 16:40:39 +0100

    Code Cleanup, Regions, Code reformated

--- start.js
+++ start.js
@@ -1,95 +1,101 @@
+//region Collections
 Sessions = new Mongo.Collection("sessions");
 Questions = new Mongo.Collection("questions");
+//endregion
 
+//region Client Code
 if (Meteor.isClient) {
-    // counter starts at 0
-    Session.setDefault('counter', 0);
-
-    Handlebars.registerHelper("isNull", function(value) {
-        return value === null;
-    });
-    Handlebars.registerHelper("isEmpty", function(object) {
-        return jQuery.isEmptyObject(object);
-    });
-    Handlebars.registerHelper("log", function(object) {
+    //region Handlebars
+    Handlebars.registerHelper("log", function (object) {
         return console.log(object);
     });
+    //endregion
 
+    //region Template Body
     Template.body.helpers({
-        sessionExist: function() {
-            if(location.search.split('sessionId=')[1]) {
+        sessionExist: function () {
+            if (location.search.split('sessionId=')[1]) {
                 var sessionId = location.search.split('sessionId=')[1];
-                return Sessions.find({sessionId : sessionId}).count();
+                return Sessions.find({sessionId: sessionId}).count();
             }
             return (Sessions.find({owner: Meteor.userId()})).count();
         }
     });
 
     Template.body.events({
-        "click .btnCreate": function() {
+        "click .btnCreate": function () {
             Meteor.call("createSession");
         },
-        "submit #formJoin": function() {
+        "submit #formJoin": function () {
             var sessionId = event.target.sessionId.value;
             Meteor.call("joinSession", sessionId);
         }
     });
+    //endregion
 
+    //region Template sessionCreated
     Template.sessionCreated.helpers({
-        session: function() {
-            if(location.search.split('sessionId=')[1]) {
+        session: function () {
+            if (location.search.split('sessionId=')[1]) {
                 var sessionId = location.search.split('sessionId=')[1];
-                return Sessions.find({sessionId : sessionId});
+                return Sessions.find({sessionId: sessionId});
             }
             return Sessions.find({owner: Meteor.userId()})
         },
-        isOwner: function() {
+        isOwner: function () {
             return this.owner === Meteor.userId();
         },
-        questions: function() {
+        questions: function () {
             return Questions.find({owner: this.owner})
         }
     });
 
     Template.sessionCreated.events({
-        "click .btnDelete": function() {
+        "click .btnDelete": function () {
             Meteor.call("terminateSession");
         },
-        "click .btnLeave": function() {
+        "click .btnLeave": function () {
             Meteor.call("leaveSession");
         },
-        "submit #formQuestion": function(event) {
+        "submit #formQuestion": function (event) {
             event.preventDefault();
             var text = event.target.questionTxt.value;
             Meteor.call("addQuestion", text, this.owner);
         }
     });
+    //endregion
 
+    //region Template questionTemplate
     Template.questionTemplate.helpers({
-        isOwner: function() {
+        isOwner: function () {
             return this.owner === Meteor.userId();
         }
     });
 
     Template.questionTemplate.events({
-       "click .btnDelQuestion": function() {
-           Meteor.call("delQuestion", this._id);
-       }
+        "click .btnDelQuestion": function () {
+            Meteor.call("delQuestion", this._id);
+        }
     });
+    //endregion
 
+    //region Account Config
     Accounts.ui.config({
         passwordSignupFields: "USERNAME_ONLY"
     });
+    //endregion
 }
+//endregion
 
+//region Server Code
 if (Meteor.isServer) {
     Meteor.startup(function () {
         // code to run on server at startup
     });
 
     Meteor.methods({
-        createSession: function() {
-            if(! Meteor.userId()) {
+        createSession: function () {
+            if (!Meteor.userId()) {
                 throw new Meteor.Error("not-authorized");
             }
             Meteor.call("terminateSession");
@@ -102,38 +108,42 @@ if (Meteor.isServer) {
                 username: Meteor.user().username
             });
         },
-        terminateSession: function() {
-            Sessions.remove({owner : Meteor.userId()})
+        terminateSession: function () {
+            Sessions.remove({owner: Meteor.userId()})
         },
-        getRandomId: function() {
-            if(! Meteor.userId()) {
+        getRandomId: function () {
+            if (!Meteor.userId()) {
                 throw new Meteor.Error("not-authorized");
             }
 
             var text = "";
             var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-            for( var i=0; i < 5; i++ )
+            for (var i = 0; i < 5; i++)
                 text += possible.charAt(Math.floor(Math.random() * possible.length));
             return text;
         }
     });
 }
+//endregion
+
+//region Methods
 Meteor.methods({
-    joinSession: function(sessionId) {
-        return Sessions.find({sessionId : sessionId});
+    joinSession: function (sessionId) {
+        return Sessions.find({sessionId: sessionId});
     },
-    leaveSession: function() {
-        location.href="/";
+    leaveSession: function () {
+        location.href = "/";
     },
-    addQuestion: function(text, owner) {
+    addQuestion: function (text, owner) {
         Questions.insert({
             owner: owner,
             type: text,
             question: text
         })
     },
-    delQuestion: function(id) {
-        Questions.remove({_id : id});
+    delQuestion: function (id) {
+        Questions.remove({_id: id});
     }
 });
+//endregion
